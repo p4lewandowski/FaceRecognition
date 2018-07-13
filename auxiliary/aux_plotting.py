78dfb41a003d39971541bbf919fa1887a8feb64b
Virnael <p4lewandowski@gmail.com>
2018-07-13 12:32:38 +0200

    Reconstruction_manual as a separate function

--- auxiliary/aux_plotting.py
+++ auxiliary/aux_plotting.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import time
 import os
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-import matplotlib.image as mpimg
 import cv2 as cv
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 
 rootdir = os.getcwd()
@@ -99,4 +99,77 @@ def compare_plot(im1, im2):
     ax2.imshow(im2, cmap=plt.cm.bone)
 
     plt.suptitle('Image comparison', fontsize=14)
+    plt.show()
+
+def reconstruction_manual(mean_img, eigenfaces, face_weights):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4))
+    plt.subplots_adjust(right=0.65)
+    ax1.imshow(mean_img, cmap=plt.cm.bone)
+
+    ax1_e = plt.axes([0.77, 0.7, 0.15, 0.03])
+    ax2_e = plt.axes([0.77, 0.65, 0.15, 0.03])
+    ax3_e = plt.axes([0.77, 0.6, 0.15, 0.03])
+    ax4_e = plt.axes([0.77, 0.55, 0.15, 0.03])
+    ax5_e = plt.axes([0.77, 0.5, 0.15, 0.03])
+    ax6_e = plt.axes([0.77, 0.45, 0.15, 0.03])
+    ax7_e = plt.axes([0.77, 0.4, 0.15, 0.03])
+    ax8_e = plt.axes([0.77, 0.35, 0.15, 0.03])
+    ax9_e = plt.axes([0.77, 0.3, 0.15, 0.03])
+
+    # mean face to weights
+    mean_face_9pca = np.matmul(face_weights[0], eigenfaces.reshape(111, 86*86))[:9]
+
+    # Back
+    reconstructed_face_template = mean_img
+    for iter, eigenface in enumerate(eigenfaces[:9]):
+        reconstructed_face_template += np.dot(mean_face_9pca[iter], eigenface)
+    ax2.imshow(reconstructed_face_template, cmap=plt.cm.bone)
+
+    slider_1 = Slider(ax1_e, 'weight 1', -5000, 5000, valinit=mean_face_9pca[0], valfmt="%.0f")
+    slider_2 = Slider(ax2_e, 'weight 2', -5000, 5000, valinit=mean_face_9pca[1], valfmt="%.0f")
+    slider_3 = Slider(ax3_e, 'weight 3', -5000, 5000, valinit=mean_face_9pca[2], valfmt="%.0f")
+    slider_4 = Slider(ax4_e, 'weight 4', -5000, 5000, valinit=mean_face_9pca[3], valfmt="%.0f")
+    slider_5 = Slider(ax5_e, 'weight 5', -5000, 5000, valinit=mean_face_9pca[4], valfmt="%.0f")
+    slider_6 = Slider(ax6_e, 'weight 6', -5000, 5000, valinit=mean_face_9pca[5], valfmt="%.0f")
+    slider_7 = Slider(ax7_e, 'weight 7', -5000, 5000, valinit=mean_face_9pca[6], valfmt="%.0f")
+    slider_8 = Slider(ax8_e, 'weight 8', -5000, 5000, valinit=mean_face_9pca[7], valfmt="%.0f")
+    slider_9 = Slider(ax9_e, 'weight 9', -5000, 5000, valinit=mean_face_9pca[8], valfmt="%.0f")
+
+    def update(val):
+        mean_face_9pca = [slider_1.val, slider_2.val, slider_3.val, slider_4.val,
+                          slider_5.val, slider_6.val, slider_7.val, slider_8.val,
+                          slider_9.val]
+
+        reconstructed_face = np.copy(mean_img)
+        for iter, eigenface in enumerate(eigenfaces[:9]):
+            reconstructed_face += np.dot(mean_face_9pca[iter], eigenface)
+
+        ax2.imshow(reconstructed_face, cmap=plt.cm.bone)
+
+    slider_1.on_changed(update);
+    slider_2.on_changed(update);
+    slider_3.on_changed(update)
+    slider_4.on_changed(update);
+    slider_5.on_changed(update);
+    slider_6.on_changed(update)
+    slider_7.on_changed(update);
+    slider_8.on_changed(update);
+    slider_9.on_changed(update)
+
+    def reset(event):
+        slider_1.reset();
+        slider_2.reset();
+        slider_3.reset();
+        slider_4.reset();
+        slider_5.reset();
+        slider_6.reset();
+        slider_7.reset();
+        slider_8.reset();
+        slider_9.reset();
+        ax2.imshow(reconstructed_face_template, cmap=plt.cm.bone)
+
+    resetax = plt.axes([0.45, 0.05, 0.1, 0.1])
+    button = Button(resetax, 'Reset', hovercolor='0.975')
+    button.on_clicked(reset)
+
     plt.show()
